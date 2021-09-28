50b3139752a0270e6ffc4b646e4849a7fa326e85
Pg7Pedro <Pg91999@hotmail.com>
2021-09-28 11:58:15 +0200

    GestionVehiculos

---
+++ GestionVehiculo/src/com/ceep/domain/Vehiculo.java
@@ -0,0 +1,70 @@
+package com.ceep.domain;
+
+public class Vehiculo {
+    
+    private String matricula,marca,modelo,color;
+    private double tarifa;
+    private boolean disponible;
+   
+  
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getTarifa() {
+        return tarifa;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setTarifa(double tarifa) {
+        this.tarifa = tarifa;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    public Vehiculo(String matricula, String marca, String modelo, String color, double tarifa, boolean disponible) {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = color;
+        this.tarifa = tarifa;
+        
+        this.disponible = disponible;
+    }
+
+    
+    
+    
+    
+    public void mostrar()
+    {
+        System.out.println("Matricula : "+matricula);
+        System.out.println("Marca : "+ marca);
+        System.out.println("Modelo : "+modelo);
+        System.out.println("Color : "+color);
+        System.out.println("Tarifa: "+tarifa);
+        System.out.println("Disponible: "+disponible);
+    
+    }
+    
+    
+    
+}
---
+++ GestionVehiculo/src/com/ceep/test/PruebaGestion.java
@@ -0,0 +1,14 @@
+package com.ceep.test;
+
+import com.ceep.domain.Vehiculo;
+
+public class PruebaGestion {
+    
+    
+    public static void main(String[] args) {
+        
+        Vehiculo V1 = new Vehiculo ("2345JBF","AUDI","RS3","Verde",3999.99,true);
+        
+    }
+    
+}

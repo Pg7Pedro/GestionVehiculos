745d03154e358c0a9d4a9186237488800db720e9
Pg7Pedro <Pg91999@hotmail.com>
2021-09-28 12:11:21 +0200

    Update PruebaGestion.java

--- GestionVehiculo/src/com/ceep/test/PruebaGestion.java
+++ GestionVehiculo/src/com/ceep/test/PruebaGestion.java
@@ -7,7 +7,7 @@ public class PruebaGestion {
     
     public static void main(String[] args) {
         
-        Vehiculo V1 = new Vehiculo ("2345JBF","AUDI","RS3","Verde",3999.99,true);
+        Vehiculo Coche1 = new Vehiculo ("2345JBF","AUDI","RS3","Verde",3999.99,true);
         
     }
     
